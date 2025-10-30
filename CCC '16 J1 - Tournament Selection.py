2783de72d34d2097cfba1a014701641900bd7ae0
Ryan <ryanxipingzhong@gmail.com>
2025-10-30 12:36:36 -0400

    16 j1

---
+++ CCC '16 J1 - Tournament Selection.py
@@ -0,0 +1,13 @@
+w = 0
+for i in range(6):
+    a = input("")
+    if a == "W":
+        w +=1
+if w >=5:
+    print(1)
+elif w == 3 or w==4:
+    print(2)
+elif w == 1 or w==2:
+    print(3)
+else:
+    print(-1)
