a4a76dbaf9a00d574100c4974bf10a26dae98a1f
Ryan <ryanxipingzhong@gmail.com>
2025-10-30 10:00:49 -0400

    11 j1

---
+++ CCC '11 J1 - Which Alien?.py
@@ -0,0 +1,18 @@
+def alien1(a,b):
+    if a>=3 and b<=4:
+        print("TroyMartian")
+    return ""
+def alien2(m,n):
+    if m<=6 and n >=2:
+        print("VladSaturnian")
+    return ""
+def alien3(x,y):
+    if x <=2 and y<=3:
+        print("GraemeMercurian")
+    return ""
+
+i = int(input())
+j = int(input())
+alien1(i,j)
+alien2(i,j)
+alien3(i,j)
