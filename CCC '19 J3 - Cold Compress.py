f6de0159b378568c76ba305380df177650281a30
Ryan <ryanxipingzhong@gmail.com>
2025-11-11 14:12:19 -0500

    j3 finsiiehds

--- CCC '19 J3 - Cold Compress.py
+++ CCC '19 J3 - Cold Compress.py
@@ -1,9 +1,22 @@
-times = int(input())
+a = int(input())
+count = 1
+lst = []
+unique = []
+result = ""
 
-
-rep = ""
-compress = ''
-
-for i in range(times):
-    string = input()
-    for j in range(len(string)):
+for i in range(a):
+    b = input()
+    b = b + " "
+    for j in range(len(b)-1):
+        if b[j] == b[j+1]:
+            count += 1
+        else:
+            lst.append(count)
+            unique.append(b[j])
+            count = 1
+    for m in range(len(lst)):
+        result += str(lst[m]) + " " + str(unique[m]) + " "
+    print(result.strip())
+    result = ""
+    lst = []
+    unique = []
