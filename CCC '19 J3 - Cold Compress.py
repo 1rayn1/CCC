60a07e8033b6357312a232d6cf7006f28fe9abb1
Ryan <ryanxipingzhong@gmail.com>
2025-11-11 14:05:47 -0500

    j3 + hci. cold compress not done

---
+++ CCC '19 J3 - Cold Compress.py
@@ -0,0 +1,9 @@
+times = int(input())
+
+
+rep = ""
+compress = ''
+
+for i in range(times):
+    string = input()
+    for j in range(len(string)):
---
+++ HCI '16 - Oversleep.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+
+ROWS, COLS = map(int, input().split())
+dir_4 = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+grid = [list(input()) for _ in range(ROWS)]#we love list comprehension
+
+for i in range(ROWS):
+    for j in range(COLS):
+        if grid[i][j] == "s":
+            q = deque([(0, i, j)])
+        elif grid[i][j] == "e":
+            end = (i,j)
+
+while q:
+    dist,row,col = q.popleft()
+    if (row,col) == end:
+        print(dist - 1)
+        sys.exit()
+    
+    for dr, dc in dir_4:
+        new_r, new_c = row + dr, col + dc
+        if 0 <= new_r < ROWS and 0 <= new_c < COLS and grid[new_r][new_c] != "X":
+            grid[new_r][new_c] = "X"
+            q.append((dist + 1, new_r, new_c))
+
+print(-1)
